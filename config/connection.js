30aad0ce10bf1cc1949089c6cccf76b155bff478
Hilwood Chan <hillwoodc@gmail.com>
2019-03-27 17:08:05 -0700

    edited

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+   }  else{
+
+ connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "!!UCLA!!",
+  database: "burger_db"
+});
+};
+
+// Make connection.
+connection.connect();
+console.log(connection);
+
+// Export connection for our ORM to use.
+module.exports = connection;
